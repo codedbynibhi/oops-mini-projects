02b1b1016bb44ee9c94d7cb3b01ac3e17167287f
Nibhi Khanna <Khannanibhi@gmail.com>
2026-07-18 16:39:00 +0530

    add assignment

---
+++ assignment.java
@@ -0,0 +1,89 @@
+// import java.util.Scanner;
+// public class assignment {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         if(n%2==0){
+//             System.out.println("even");
+//         }
+//         else{
+//             System.out.println("odd");
+//         }
+//         sc.close();
+        
+//     }
+    
+// }
+//------------------------------------------
+// import java.util.Scanner;
+// public class assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         for(int i=1;i<=10;i++){
+//             System.out.println(n + "*" + i +"="+ n*i);
+//         }
+//     }
+// }
+//-----------------------
+// import java.util.Scanner;
+//     public class assignment{
+        
+// public static void main(String[] args){
+//                Scanner sc = new Scanner(System.in);
+
+//         int start = sc.nextInt();
+//         int end = sc.nextInt();
+//         for(int num=start;num<=end;num++){
+//             if(num<=1){
+//                 continue;
+//         }
+//         boolean isprime =true;
+//   for (int i = 2; i * i <= num; i++) {
+//                 if (num % i == 0) {
+//                     isprime = false;
+//                     break;
+// }
+//     }
+//     if (isprime) {
+//                 System.out.println(num);
+//             }
+//         }
+
+//         sc.close();
+//     }
+// } 
+
+// import java.util.Scanner;
+// public class assignment{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int n = 27;
+//         for(int i=1;i<=5;i++){
+//             int num = sc.nextInt();
+//             if(num<10){
+//                 System.out.println("too low");
+//             }
+//             else if(num<20){
+//                 System.out.println("low");
+//             }
+//             else if(num==27){
+//                 System.out.println("congratulation");
+//             }
+            
+//         }
+//     }
+// }
+import java.util.Scanner;
+public class assignment{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int original = num;
+        int sum=0;
+        while(num>0){
+            int digit = num%10;
+            int fact =1;
+        }
+    }
+}
