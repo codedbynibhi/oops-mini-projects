8efdf97c3c64d65935106238d5a3495384cdad9d
codedbynibhi <khannanibhi@gmail.com>
2026-07-18 11:33:51 +0530

    Delete static.java

--- static.java
+++
@@ -1,7 +0,0 @@
-public class static {
-    
-}
-//Static variable = School name (same for every student)
-//Non-static variable = Student's roll number (different for every student)
-//A non-static method can access both static and non-static members, whereas a static method can directly access only static members because it belongs to the class and has no reference to any specific object.
-//remeber the static and non static eg the one in which mam showd drawing of college chitkara and students wiith their name and roll
